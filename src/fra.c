d8b57d9a6e3a160c031d6f337532742d2b06f030
Qusarun <94105803+Qusarun@users.noreply.github.com>
2022-03-09 21:57:51 +0300

    Update fra.c

--- src/fra.c
+++ src/fra.c
@@ -1,9 +1,9 @@
 #include "../include/fra.h"
 
-bool roll(double chance, FRA* fra) {
+bool roll(double chance, double* d) {
     bool a = rand() % 100000 > chance * 100000;
-    fra->d += a? -chance : chance;
-    bool b = fra->d > chance * 2 || (a && fra->d > chance * -2);
-    fra->d += b? -chance : chance;
+    d[0] += a? -chance : chance;
+    bool b = d[0] > chance * 2 || (a && d[0] > chance * -2);
+    d[0] += b? -chance : chance;
     return b;
 }
